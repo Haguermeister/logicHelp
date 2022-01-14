b04b2ed3c1e083f19f969f6d71dda4e9a4b4632c
Haguermeister <cadethague70@gmail.com>
2022-01-14 13:28:45 -0500

    Line 76

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,7 +72,7 @@ ref = findMixinIndex("Egg");
 
 const avaiable_ing = new Set(['Kahlua', 'Sambuca', 'Grand Marnier', 'Vodka']);
 var test = drinkRecipes.filter(cocktail => {
-    cocktail.ingArray.every(avaiable_ing.has());
+    cocktail.ingArray.every(array => array == avaiable_ing.has());
     console.log(cocktail.ingArray);
 })
 console.log(test);
